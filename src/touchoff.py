9b63e495890a17f82eab62401669f368db77ada1
James Haley <haleyjc@ornl.gov>
2023-08-26 15:41:38 -0400

    todo notes

--- src/touchoff.py
+++ src/touchoff.py
@@ -12,6 +12,19 @@ TODO:
 - get ctrl+c to kill motion, or another escape. Right now needs to be estopped and restarted
 - USE CALIBRATION MANAGER
 - publish a box visual on touchoffs? 
+- permit calling as service
+- make a rqt plugin for buttons
+- make a ros-mobile integration
+
+TODO: new touchoff macros...
+- internal corner
+- centerpoint of hole
+- centerpoint of rod
+- TCP 4 point??
+- many point angle
+- many point plane
+- Edge 
+- corner + edge
 '''
 # Python imports
 import copy
@@ -640,51 +653,3 @@ Quit with ctrl+c or q'''
             break
         else:
             print(help)
-
-'''TODO:
-- centerpoint of hole
-- centerpoint of rod
-- TCP??
-- many point angle
-- many point plane
-- Edge 
-- Corner
-'''
-
-
-
-        # ATTEMPTING PILZ
-        # self.r.move(Lin(goal=Pose(position=Point(0, 0, 0.01)), planning_group='arm', reference_frame="prbt_tcp", vel_scale=0.2))
-        # self.r.move(Lin(goal=Pose(position=Point(0, 0, 0.01)), planning_group='arm', reference_frame="robot_tcp", vel_scale=0.2))
-        # reference_frame="robot_tcp"
-        # target_link='robot_tcp',base='world'
-
-        # PTP
-        # self.move_group.clear_pose_targets()
-        # self.move_group.set_pose_target(new_pose)
-        # plan = self.move_group.plan()
-        # print(plan)
-        # success = self.move_group.execute(plan, wait=wait)
-        # self.move_group.clear_pose_targets()
-
-
-
-# '''
-# uint8 PENDING         = 0   # The goal has yet to be processed by the action server
-# uint8 ACTIVE          = 1   # The goal is currently being processed by the action server
-# uint8 PREEMPTED       = 2   # The goal received a cancel request after it started executing
-#                             #   and has since completed its execution (Terminal State)
-# uint8 SUCCEEDED       = 3   # The goal was achieved successfully by the action server (Terminal State)
-# uint8 ABORTED         = 4   # The goal was aborted during execution by the action server due
-#                             #    to some failure (Terminal State)
-# uint8 REJECTED        = 5   # The goal was rejected by the action server without being processed,
-#                             #    because the goal was unattainable or invalid (Terminal State)
-# uint8 PREEMPTING      = 6   # The goal received a cancel request after it started executing
-#                             #    and has not yet completed execution
-# uint8 RECALLING       = 7   # The goal received a cancel request before it started executing,
-#                             #    but the action server has not yet confirmed that the goal is canceled
-# uint8 RECALLED        = 8   # The goal received a cancel request before it started executing
-#                             #    and was successfully cancelled (Terminal State)
-# uint8 LOST            = 9   # An action client can determine that a goal is LOST. This should not be
-#                             #    sent over the wire by an action server
-# '''
